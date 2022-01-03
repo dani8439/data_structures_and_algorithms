cabc1a17fd516c03e999f2017950a9653d5bf590
Dani <dani8439@aol.com>
2022-01-03 16:01:54 -0500

    Updated for Part 2

--- advent_of_code_day1.rb
+++ advent_of_code_day1.rb
@@ -1,4 +1,5 @@
 # Advent of Code - Walk through - showing how to parse big file as argument.
+# Part 1
 
 def number_of_increases(input)
     input.each_cons(2).inject(0) do |count, (x, y)| 
@@ -15,3 +16,21 @@ input = File.readlines(ARGV.first).map(&:to_i)
 # p input 
 p number_of_increases(input)
 end 
+
+# Part 2 - sliding scale of 3 instead of 2
+
+# def number_of_increases(input)
+#     input.each_cons(3).inject(0) do |count, (x, y)| 
+#         count += 1 if x < y
+#         count 
+#     end
+# end 
+
+# def windows(n, input) 
+#     input.each_cons(3).map(&:sum)
+# end 
+
+# if __FILE__ == $0
+#     input = File.readlines(ARGV.first).map(&:to_i)
+#     p number_of_increases(windows(3, input))
+# end 
