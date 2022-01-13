e2444e70d417db715498a79d630740b00e721b56
Dani <dani8439@aol.com>
2022-01-13 12:09:55 -0500

    Practicing DSA.

--- practice.js
+++ practice.js
@@ -63,48 +63,64 @@ function maxChar(str) {
   return maxChar;
 }
 
-function reverseString(str) {
-  return str.split("").reverse().join("");
-}
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
 
-function reverseString2(str) {
-  let reversed = "";
   for (let char of str) {
-    reversed = char + reversed;
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
   }
-  return reversed;
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
 }
 
-function reverseString3(str) {
-  return str.split("").reduce((rev, char) => char + rev, "");
-}
-
-function palidrome(str) {
-  const reversed = str.split("").reverse().join("");
-
-  return reversed === str;
-}
-
-function palindrome2(str) {
-  return str.split("").every((char, i) => {
-    return char === str[str.length - i - 1];
-  });
-}
-
-function reverseInt(n) {
-  const reversed = n.toString().split("").reverse().join("");
-
-  return parseInt(reversed) * Math.sign(n);
-}
-
-function reverseInt2(n) {
-  const reversed = n.toString().split("").reverse().join("");
-
-  if (n < 0) {
-    return parseInt(reversed) * -1;
-  } else {
-    return parseInt(reversed);
+function fizzBuzz(n) {
+  for (let num = 1; num <= n; num++) {
+    if (num % 3 === 0 && num % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (num % 3 === 0) {
+      console.log("fizz");
+    } else if (num % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(num);
+    }
   }
 }
 
-function maxChar(str) {}
+function chunk(array, size) {
+  const chunked = [];
+
+  for (let element of array) {
+    const last = chunked[chunked.length - 1];
+
+    if (!last || last.length === size) {
+      chunked.push([element]);
+    } else {
+      last.push(element);
+    }
+  }
+  return chunked;
+}
+
+function chunk2(array, size) {
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunked;
+}
