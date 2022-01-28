ac666f460d89358dd69b76a58f001b44e65d0aa4
Dani Schuhman <dani8439@aol.com>
2022-01-28 14:51:00 -0500

    Recall.

--- practice.js
+++ practice.js
@@ -63,6 +63,28 @@ function maxChar(str) {
   return maxChar;
 }
 
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
+
 function fizzBuzz(n) {
   for (let num = 1; num <= n; num++) {
     if (num % 3 === 0 && num % 5 === 0) {
